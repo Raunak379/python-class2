742aa9ffbed7ea15e73749b7c110ce66864aca7d
Raunak Kumar Singh <rs114408@gmail.com>
2025-07-28 13:29:20 +0530

    Add files via upload

---
+++ 2flow_control.py
@@ -0,0 +1,42 @@
+#if else statement
+
+#conditon 1
+"""number = int(input("enter your number : "))
+if number >= 0:
+    print("number is positive")"""
+
+#condition 2
+"""score = int(input("enter you score"))
+if score >= 30:
+    print("passed")
+else:
+    print("you are failed")"""
+
+#condition 3
+"""score = int(input("enter your score : "))
+if score > 100 or score < 0:
+    print("invalid result ")
+elif score >= 30:
+    print("passed")
+else:
+    print("failed")"""
+#loop contro
+
+#condition 1
+"""for i in range(4):
+    print(i)"""
+
+#condition 2
+"""number = int(input("entered your number "))
+count =0
+while count <=10:
+    product = number * count
+    print(number, "x", count, "=", product)
+    count = count +1
+"""
+#break statement
+
+#condition 1
+"""for iteam in range (1,6):
+    print(iteam)
+    break;"""
